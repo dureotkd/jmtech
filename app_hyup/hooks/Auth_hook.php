c31ae8b031017d7b6134e767822995ce57de04b4
sungmin <dureotkd123@naver.com>
2025-11-10 14:06:46 +0900

    15d

--- app_hyup/hooks/Auth_hook.php
+++ app_hyup/hooks/Auth_hook.php
@@ -8,7 +8,7 @@ function redirect_handler()
      */
 
     @session_start();
-    $uid = isset($_SESSION['uid']) ? $_SESSION['uid'] : 1;
+    $uid = isset($_SESSION['uid']) ? $_SESSION['uid'] : 0;
 
     if (empty($uid)) {
 

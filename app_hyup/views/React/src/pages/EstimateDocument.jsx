927e18f0fab20c67429b2257c1167e3d8c02330c
duerotkd <duerotkd123@naver.com>
2026-01-12 14:42:54 +0900

    fix) 공급가액 0원이하일 경우 등록안되게

--- app_hyup/views/React/src/pages/EstimateDocument.jsx
+++ app_hyup/views/React/src/pages/EstimateDocument.jsx
@@ -192,6 +192,11 @@ export default function EstimateDocument() {
         break;
     }
 
+    if (supplyAmount <= 0) {
+      alert("공급가액이 없습니다 견적서를 다시 확인해주세요.");
+      return;
+    }
+
     const cloneSheets = deepClone(sheets);
 
     cloneSheets[0].data = hots[0];
